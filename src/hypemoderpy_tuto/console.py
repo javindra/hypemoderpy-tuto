efe6eedd5e31fa2cbe6092dc42ea185eb8b51a6a
javindra <jirojo@JIROJO10.indra.es>
2021-11-09 23:03:02 +0100

    update

---
+++ src/hypemoderpy_tuto/console.py
@@ -0,0 +1,24 @@
+# src/hypemoderpy_tuto/console.py
+import textwrap
+
+import click
+import requests
+
+from . import __version__
+
+
+@click.command()
+@click.version_option(version=__version__)
+@click.option('--lang', default="en", show_default=True)
+def main(lang):
+    """The hypermodern Python project."""
+    API_URL = f"https://{lang}.wikipedia.org/api/rest_v1/page/random/summary"
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
